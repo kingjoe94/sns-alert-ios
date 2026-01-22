a2e062fe05afd25e8d1eb77b3c13834bfd6e32c0
kiyoma <kym.k.246826@gmail.com>
2026-01-23 01:06:31 +0900

    Initial Commit

---
+++ SNSalert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SNSalert
+//
+//  Created by 金城静馬 on 2026/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SNSalert/SNSalertApp.swift
@@ -0,0 +1,17 @@
+//
+//  SNSalertApp.swift
+//  SNSalert
+//
+//  Created by 金城静馬 on 2026/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SNSalertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
